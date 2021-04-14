42b9e792d69d0a052c9830e51ae4b2e4c8e90e02
Tech-Matt <mattia.rizzo.un@gmail.com>
2021-04-14 08:28:19 +0200

    Added the main core functionalities

--- gravity.pyde
+++ gravity.pyde
@@ -1,18 +1,25 @@
 #MAIN --> simulation of a particles system influenced by gravity and temperatures
 from particle import Particle
 
-num_particles = 100
+num_particles = 10
 particle_array = []
 
 def setup():
     size(1000, 600)
-    
-    for i in range(num_particles):
+    for p in range(num_particles):
         p = Particle()
-        p.display()
-        
-    
+        particle_array.append(p)
     
 def draw():
-    pass
+    background(255)
+    
+    for i in range(len(particle_array)):
+        particle_array[i].display()
+        #I'm creating another array without the particle considered in the loop. So i can confront the first one with
+        #every other particle except itself.
+        part_array = [elem for elem in particle_array if elem != particle_array[i]]
+        #check interaction with every particles
+        for k in range(len(part_array)):
+            particle_array[i].check_interaction(part_array[k])
+        particle_array[i].update()
     
--- particle.py
+++ particle.py
@@ -1,20 +1,62 @@
 screen_width = 1000
 screen_height = 600
 
+mass_min = 10**9 #minimum mass
+mass_max = 50**9 #maximum mass
+thickness_min = 1 
+thickness_max = 10 
+
+G = (6.67) * (10 ** -11) #Gravitational constant
+
 class Particle():
     def __init__(self):
         self.x = int(random(screen_width))
         self.y = int(random(screen_height))
-        self.thickness = 10 #it should be very small, a few pixels
         self.temp = 0 #Temperature of the particle, temperature will also influence the color of the particle
-        self.mass = int(random(5)) #minimum mass, 1 kg, max 5kg
+        self.mass = int(random(mass_min, mass_max)) 
         self.v = PVector.random2D() #The particle will have a random velocity
+        self.a = PVector(0, 0, 0) #Resulting acceleration due to gravity interactions
         
     def display(self):
+        self.thickness = map(self.mass, mass_min, mass_max, thickness_min, thickness_max)#Mapping size based on mass
         fill(30, 209, 48)
         circle(self.x, self.y, self.thickness)
+    
+    def check_interaction(self, p2): #Determine the force and verse of the force of gravity between 2 particles
+        m1 = self.mass
+        m2 = p2.mass
+        x1 = self.x
+        y1 = self.y
+        x2 = p2.x
+        y2 = p2.y
+        print("x1: " + str(x1), "x2: " + str(x2), "y1: " + str(y1), "y2: " + str(y2))
+        r = int(sqrt(((x2 - x1)**2) + ((y2 - y1)**2))) #Distance between the particles
         
-    def update(self):#Update the particle coordinates and interactions with other particles
-        pass
+        #OPTIONAL --> SHOW THE INTERACTIONS -- uncomment to try
+        #if r < 100: #If distance is low show the distance
+            #stroke(110);
+            #line(x1, y1, x2, y2) #Line to visualize the distance of the interactions
+            
+        #I now cut out all the interactions with r < 1 or r > 200 because they are not relevant
+        #or cause problems
+        if r < 200 and r > 1:
+            f = G * (m1 * m2)/ (r ** 2) #Particle is subjected to this force of gravity
+            a = f / m1 #Acceleration due to force of gravity
+        
+            angle = asin((y2 - y1) / r) #Orientation of the acceleration vector
+            ax = a * cos(angle)
+            ay = a * sin(angle)
+            self.a.add(ax, ay, 0) #update Acceleration vector
+        
+        
+    def update(self):#Update the particle moovement
+        Vx = self.v.x + self.a.x #Speed x component + acceleration due to particles interactions
+        Vy = self.v.y + self.a.y #Speed y component
+        self.x += Vx
+        self.y += Vy #Costant speed
+        
+        self.a.set(0, 0, 0)#reset acceleration
+        
+    
         
         
